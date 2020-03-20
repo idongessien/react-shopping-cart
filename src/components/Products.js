ba234d3dd76e44a91c52803b33cfeb01f1be79c8
Idong Essien <idong.essien@comcast.net>
2020-03-19 23:22:47 -0400

    MVP

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,16 +4,16 @@ import React, { useContext } from 'react';
 import Product from './Product';
 import ProductContext from '../contexts/ProductContext';
 
-const Products = props => {
+const Products = () => {
 	const {products, addItem} = useContext(ProductContext);
-	
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
